ebe28754f0ebb5a5658e95498caf6861910d8393
Thomas Sibley <tsibley@cpan.org>
2014-10-12 17:09:43 -0700

    milla skeleton

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Dist::Zilla::App::Command::update - Blah blah blah
+
+# SYNOPSIS
+
+    use Dist::Zilla::App::Command::update;
+
+# DESCRIPTION
+
+Dist::Zilla::App::Command::update is
+
+# AUTHOR
+
+Thomas Sibley <tsibley@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2014- Thomas Sibley
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Dist/Zilla/App/Command/update.pm
@@ -0,0 +1,39 @@
+package Dist::Zilla::App::Command::update;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Dist::Zilla::App::Command::update - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Dist::Zilla::App::Command::update;
+
+=head1 DESCRIPTION
+
+Dist::Zilla::App::Command::update is
+
+=head1 AUTHOR
+
+Thomas Sibley E<lt>tsibley@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Thomas Sibley
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
